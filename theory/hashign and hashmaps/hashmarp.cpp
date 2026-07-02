03283d732bd717adb5037e9228fd945706966e0a
smarpitm <smarpitmalik@gmail.com>
2026-07-02 12:42:57 +0530

    hashmap learnt

---
+++ theory/hashign and hashmaps/hashmarp.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    { cin >> arr[i];
+       }
+
+    //
+       int hash[130]={0};
+       for (int i = 0; i < n;i++)
+       {
+           hash[arr[i]] += 1;
+       }
+
+           int q;
+       cin >> q;
+       while (q --)
+       {
+           int num;
+           cin >> num;
+           // fetch
+           cout << hash[num] << endl;
+       }
+}
